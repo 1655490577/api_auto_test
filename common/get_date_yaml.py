ef2f0781a65501dc37de0832e5fffe0c2c40a2d0
yangtong <12345678>
2020-06-16 11:59:00 +0800

    123
    Signed-off-by: yangtong <12345678>

--- common/get_date_yaml.py
+++ common/get_date_yaml.py
@@ -1,19 +1,16 @@
 import yaml
 
-with open('../Date/test.yaml', encoding='utf-8') as f:
-    print(yaml.safe_load(f))
 
-# yaml_str = '''
-# name: Cactus
-# age: 18
-# skills:
-#   -
-#     - Python
-#     - 3
-#   -
-#     - Java
-#     - 5
-# has_blog: true
-# gf: ~
-# '''
-# print(yaml.safe_load(yaml_str))
+class ReadFileData():
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def load_yaml(file_path):
+        with open(file_path, encoding='utf-8') as f:
+            read_data = yaml.safe_load(f)
+        return read_data
+
+
+data = ReadFileData()
--- test_case/test_001_login.py
+++ test_case/test_001_login.py
@@ -1,5 +1,6 @@
 import requests
 import pytest
+from conftest import api_date
 
 url = 'http://192.168.30.11:10060/supervisor' + '/admin/sysadmin/login'
 headers = {'Content-Type': 'application/json'}
@@ -10,7 +11,7 @@ json = {
 }
 
 
-@pytest.mark.parametrize('phone, password', [('admin', 'admin'), ('13168775547', '123456'), ('13168775548', '123456')])
+@pytest.mark.parametrize('phone, password', api_date["login_success"])
 def test_login_001(phone, password):
     """
     参数可以登录成功(管理员账号)
@@ -29,22 +30,22 @@ def test_login_001(phone, password):
     assert '单位列表', '参建单位列表' in r.text
 
 
-@pytest.mark.parametrize('phone, password', [(' ', ' '), ('admin', '123'), ('13168775547', None)])
-def test_login_002(phone, password):
-    """
-    参数登录失败
-    :param phone: 登录手机号
-    :param password: 登录密码
-    :return:
-    """
-    json_date = {
-        "password": password,
-        "phone": phone,
-        "rememberMe": True
-    }
-    r = requests.post(url=url, json=json_date, headers=headers)
-    assert r.status_code == 200
-    assert r.json()['date'] == 0
+# @pytest.mark.parametrize('phone, password', [(' ', ' '), ('admin', '123'), ('13168775547', None)])
+# def test_login_002(phone, password):
+#     """
+#     参数登录失败
+#     :param phone: 登录手机号
+#     :param password: 登录密码
+#     :return:
+#     """
+#     json_date = {
+#         "password": password,
+#         "phone": phone,
+#         "rememberMe": True
+#     }
+#     r = requests.post(url=url, json=json_date, headers=headers)
+#     assert r.status_code == 200
+#     assert r.json()['date'] == 0
 
 
 if __name__ == '__main__':
