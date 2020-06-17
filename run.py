10004a78da6dae5afdc2305ff1b914e9593b7dcd
yangtong <12345678>
2020-06-17 18:38:05 +0800

    Initial commit
    Signed-off-by: yangtong <12345678>

--- run.py
+++ run.py
@@ -3,5 +3,6 @@ import os
 
 
 if __name__ == '__main__':
-    pytest.main(['--alluredir', './temp'])
-    os.system('allure generate ./temp/ -o ./report/ --clean')
+    # pytest.main(['--alluredir', './temp'])
+    # os.system('allure generate ./temp/ -o ./report/ --clean')
+    pytest.main()
