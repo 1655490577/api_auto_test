63db93f8874a2da47be31a98f0ae6a5dacc5fe86
yangtong <12345678>
2020-06-29 17:57:44 +0800

    update
    Signed-off-by: yangtong <12345678>

--- test_case/test_002_change_password.py
+++ test_case/test_002_change_password.py
@@ -7,8 +7,10 @@ from api.api_public_method import change_password, login
 
 class Test_change_password(object):
 
+    @pytest.mark.skipif(login(phone="admin", password="admin", rememberMe=True).json()["status"] != "0",
+                        reason="admin用户登录失败")
     @pytest.mark.parametrize('id,name,change_pwd,change_phone,login_pwd,login_phone,userId',
-                             get_data('change_password_data.yml')["test_change_password_01"])
+                             get_data('change_password_data.yml')["test_change_password_success"])
     def test_update_password_system(self, id, name, change_pwd, change_phone, login_pwd, login_phone, userId):
         """
         用例描述：
@@ -28,9 +30,10 @@ class Test_change_password(object):
                                  phone=login_phone, token=token, userid=userId)
         with allure.step("step3: 步骤3 ==>> 被修改的用户使用新密码登录系统"):
             r2 = login(phone=change_phone, password=change_pwd + '123', rememberMe=True)
-        if login_pwd == change_pwd:
+        if login_phone == change_phone:
             with allure.step("step4: 重新登录并获取token,cookies"):
-                token2, cookies2 = get_login_token_cookies(phone=login_phone, password=login_pwd+"123", rememberMe=True)
+                token2, cookies2 = get_login_token_cookies(phone=login_phone, password=login_pwd + "123",
+                                                           rememberMe=True)
             with allure.step("step5: 步骤5 ==>> 用户再把自己的密码由原密码+‘123’变回原密码"):
                 r3 = change_password(cookies2, id=id, name=name, password=login_pwd, phone=login_phone,
                                      token=token2, userid=userId)
@@ -38,7 +41,7 @@ class Test_change_password(object):
             with allure.step("step4: 重新登录并获取token,cookies"):
                 token2, cookies2 = get_login_token_cookies(phone=login_phone, password=login_pwd, rememberMe=True)
             with allure.step("step5: 步骤5 ==>> 系统管理员再把各用户的密码由原密码+‘123’变回原密码"):
-                r3 = change_password(cookies2, id=id, name=name, password=login_pwd, phone=login_phone,
+                r3 = change_password(cookies2, id=id, name=name, password=change_pwd, phone=login_phone,
                                      token=token2, userid=userId)
 
         assert r1.status_code == 200
