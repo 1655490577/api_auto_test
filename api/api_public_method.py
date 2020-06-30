d708e716519121c587ebfac5c2e48a9eea04bfc1
yangtong <12345678>
2020-06-30 11:44:22 +0800

    update
    Signed-off-by: yangtong <12345678>

--- api/api_public_method.py
+++ api/api_public_method.py
@@ -11,10 +11,10 @@ class User(object):
     def login(self, **kwargs):
         return requests.post(url=self.ip + '/admin/sysadmin/login', json=kwargs, headers=self.headers)
 
-    def change_password(self, cookies, **kwargs):
+    def update(self, cookies, **kwargs):
         return requests.post(url=self.ip + '/admin/sysadmin/update', json=kwargs, headers=self.headers, cookies=cookies)
 
-    def add_user(self, cookies, **kwargs):
+    def save(self, cookies, **kwargs):
         return requests.post(url=self.ip + '/admin/sysadmin/save', json=kwargs, headers=self.headers, cookies=cookies)
 
 
--- api/beforeTest.py
+++ api/beforeTest.py
@@ -32,41 +32,41 @@ def add_test_user():
 
     token, cookies = get_login_token_cookies("admin", "admin", True)
 
-    user.add_user(cookies, dataType=1, isAdmin=1, name="测试系统管理员01", password="123456", phone="13168775501",
-                  roleId="259752668312895488", state=1, token=token, userid="259098505857990656")  # 添加测试系统管理员01
+    user.save(cookies, dataType=1, isAdmin=1, name="测试系统管理员01", password="123456", phone="13168775501",
+              roleId="259752668312895488", state=1, token=token, userid="259098505857990656")  # 添加测试系统管理员01
 
-    user.add_user(cookies, dataType=1, isAdmin=1, name="测试系统管理员02", password="123456", phone="13168775502",
-                  roleId="259752668312895488", state=1, token=token, userid="259098505857990656")  # 添加测试系统管理员02
+    user.save(cookies, dataType=1, isAdmin=1, name="测试系统管理员02", password="123456", phone="13168775502",
+              roleId="259752668312895488", state=1, token=token, userid="259098505857990656")  # 添加测试系统管理员02
 
-    user.add_user(cookies, dataType=1, isAdmin=2, name="测试公司管理员01", password="123456", phone="13168775503",
-                  roleId="259821125242978304", state=1, token=token, userid="259098505857990656",
-                  dicCode="0102", dicId="259718019809280000", dicName="测试公司")  # 添加测试公司管理员01
+    user.save(cookies, dataType=1, isAdmin=2, name="测试公司管理员01", password="123456", phone="13168775503",
+              roleId="259821125242978304", state=1, token=token, userid="259098505857990656",
+              dicCode="0102", dicId="259718019809280000", dicName="测试公司")  # 添加测试公司管理员01
 
-    user.add_user(cookies, dataType=1, isAdmin=2, name="测试公司管理员02", password="123456", phone="13168775504",
-                  roleId="259821125242978304", state=1, token=token, userid="259098505857990656",
-                  dicCode="0102", dicId="259718019809280000", dicName="测试公司")  # 添加测试公司管理员02
+    user.save(cookies, dataType=1, isAdmin=2, name="测试公司管理员02", password="123456", phone="13168775504",
+              roleId="259821125242978304", state=1, token=token, userid="259098505857990656",
+              dicCode="0102", dicId="259718019809280000", dicName="测试公司")  # 添加测试公司管理员02
 
-    user.add_user(cookies, dataType=1, isAdmin=2, name="测试部门用户01", password="123456", phone="13168775505",
-                  roleId="259720390136299520 ", state=1, token=token, userid="259098505857990656",
-                  branchCode="010201", branchId="259718102269296640", branchName="工程部01", dicCode="0102",
-                  dicId="259718019809280000", dicName="测试公司")  # 添加测试部门用户01
+    user.save(cookies, dataType=1, isAdmin=2, name="测试部门用户01", password="123456", phone="13168775505",
+              roleId="259720390136299520 ", state=1, token=token, userid="259098505857990656",
+              branchCode="010201", branchId="259718102269296640", branchName="工程部01", dicCode="0102",
+              dicId="259718019809280000", dicName="测试公司")  # 添加测试部门用户01
 
-    user.add_user(cookies, dataType=1, isAdmin=2, name="测试部门用户02", password="123456", phone="13168775506",
-                  roleId="259720390136299520 ", state=1, token=token, userid="259098505857990656",
-                  branchCode="010201", branchId="259718102269296640", branchName="工程部01", dicCode="0102",
-                  dicId="259718019809280000", dicName="测试公司")  # 添加测试部门用户02
+    user.save(cookies, dataType=1, isAdmin=2, name="测试部门用户02", password="123456", phone="13168775506",
+              roleId="259720390136299520 ", state=1, token=token, userid="259098505857990656",
+              branchCode="010201", branchId="259718102269296640", branchName="工程部01", dicCode="0102",
+              dicId="259718019809280000", dicName="测试公司")  # 添加测试部门用户02
 
-    user.add_user(cookies, dataType=1, isAdmin=2, name="测试组用户01", password="123456", phone="13168775507",
-                  roleId="259720526056914944 ", state=1, token=token, userid="259098505857990656",
-                  branchCode="010201", branchId="259718102269296640", branchName="工程部01", dicCode="0102",
-                  dicId="259718019809280000", dicName="测试公司", groupCode="01020101", groupId="259718194518818816",
-                  groupName="事业组01")  # 添加测试组用户01
+    user.save(cookies, dataType=1, isAdmin=2, name="测试组用户01", password="123456", phone="13168775507",
+              roleId="259720526056914944 ", state=1, token=token, userid="259098505857990656",
+              branchCode="010201", branchId="259718102269296640", branchName="工程部01", dicCode="0102",
+              dicId="259718019809280000", dicName="测试公司", groupCode="01020101", groupId="259718194518818816",
+              groupName="事业组01")  # 添加测试组用户01
 
-    user.add_user(cookies, dataType=1, isAdmin=2, name="测试组用户02", password="123456", phone="13168775508",
-                  roleId="259720526056914944 ", state=1, token=token, userid="259098505857990656",
-                  branchCode="010201", branchId="259718102269296640", branchName="工程部01", dicCode="0102",
-                  dicId="259718019809280000", dicName="测试公司", groupCode="01020101", groupId="259718194518818816",
-                  groupName="事业组01")  # 添加测试组用户02
+    user.save(cookies, dataType=1, isAdmin=2, name="测试组用户02", password="123456", phone="13168775508",
+              roleId="259720526056914944 ", state=1, token=token, userid="259098505857990656",
+              branchCode="010201", branchId="259718102269296640", branchName="工程部01", dicCode="0102",
+              dicId="259718019809280000", dicName="测试公司", groupCode="01020101", groupId="259718194518818816",
+              groupName="事业组01")  # 添加测试组用户02
 
 
 if __name__ == '__main__':
